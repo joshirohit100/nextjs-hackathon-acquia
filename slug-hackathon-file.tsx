0110505f4ce6d4c2f328a4a4e35850e17d3b9b75
Rohit Joshi <joshi.rohit100@gmail.com>
2022-08-05 14:41:48 +0530

    Adding some updates

--- slug-hackathon-file.tsx
+++ slug-hackathon-file.tsx
@@ -8,7 +8,7 @@ export default function HackaThon({ menus }: LayoutProps) {
     const router = useRouter();
     const hackathonName = router.query.hackathonType;
     return (
-        <Layout title='${hackathonName}' menus={menus}>
+        <Layout title={hackathonName} menus={menus}>
             <div className="container px-6 pb-10 mx-auto">
                 Hello Welcome to the { hackathonName} Hackathon
             </div>
@@ -26,7 +26,7 @@ export async function getStaticPaths(context): Promise<GetStaticPathsResult> {
             },
             {
                 params: {
-                    hackathonType: 'NextJS',
+                    hackathonType: 'Acquia',
                 }
             }
         ],
