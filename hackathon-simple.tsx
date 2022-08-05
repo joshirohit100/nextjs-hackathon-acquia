476b32d2dfbd1b57fee4dc9358876c0fe1c3ae3a
Rohit Joshi <joshi.rohit100@gmail.com>
2022-08-05 13:58:16 +0530

    Adding simple file

---
+++ hackathon-simple.tsx
@@ -0,0 +1,7 @@
+export default function Hackathon() {
+    return (
+        <div className="container px-6 pb-10 mx-auto">
+                Hello Welcome to the Hackathon
+            </div>
+    );
+}
