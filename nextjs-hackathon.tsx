610d076ea7ab73ed8952a19ac9891ab1f4a4160c
Rohit Joshi <joshi.rohit100@gmail.com>
2022-08-05 14:31:41 +0530

    updating component name

--- nextjs-hackathon.tsx
+++ nextjs-hackathon.tsx
@@ -4,7 +4,7 @@ import {getMenus} from "../../lib/get-menus";
 
 export default function NextJsHackaThon({ menus }: LayoutProps) {
     return (
-        <Layout title="Articles" menus={menus}>
+        <Layout title="NextJS hackathon" menus={menus}>
             <div className="container px-6 pb-10 mx-auto">
                 Hello Welcome to the Next JS Hackathon
             </div>
