6e6fce1f7b49b328b9b02c30f6d49e66c23e86ad
Rohit Joshi <joshi.rohit100@gmail.com>
2022-08-05 14:31:02 +0530

    updating component name

--- hackathon-with-layout.tsx
+++ hackathon-with-layout.tsx
@@ -2,9 +2,9 @@ import { Layout, LayoutProps } from 'components/layout';
 import {GetStaticPropsResult} from "next";
 import {getMenus} from "../lib/get-menus";
 
-export default function Hackathon({ menus }: LayoutProps) {
+export default function Hackathons({ menus }: LayoutProps) {
     return (
-        <Layout title="Articles" menus={menus}>
+        <Layout title="Hackathons" menus={menus}>
             <div className="container px-6 pb-10 mx-auto">
                 Hello Welcome to the Hackathon
             </div>
