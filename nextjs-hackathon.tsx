c358f53e819a95e9a8dc46dda74b0a90af925d32
Rohit Joshi <joshi.rohit100@gmail.com>
2022-08-05 14:07:55 +0530

    nextjs hackathon

---
+++ nextjs-hackathon.tsx
@@ -0,0 +1,23 @@
+import { Layout, LayoutProps } from 'components/layout';
+import {GetStaticPropsResult} from "next";
+import {getMenus} from "../../lib/get-menus";
+
+export default function NextJsHackaThon({ menus }: LayoutProps) {
+    return (
+        <Layout title="Articles" menus={menus}>
+            <div className="container px-6 pb-10 mx-auto">
+                Hello Welcome to the Next JS Hackathon
+            </div>
+        </Layout>
+    );
+}
+
+export async function getStaticProps(
+    context,
+): Promise<GetStaticPropsResult<LayoutProps>> {
+    return {
+        props: {
+            menus: await getMenus(),
+        },
+    };
+}
