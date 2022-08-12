2c4755e923ea257f75937b2559881c4989d51100
Rohit Joshi <joshi.rohit100@gmail.com>
2022-08-12 15:07:58 +0530

    override layout

---
+++ newslist-layout-override.tsx
@@ -0,0 +1,12 @@
+NewsListPage.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <>
+            {page}
+            <CustomFooter/>
+        </>
+    )
+}
+
+
+
+// import type { ReactElement} from "react";
